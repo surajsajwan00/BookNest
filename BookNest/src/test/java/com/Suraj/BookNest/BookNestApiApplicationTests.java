4236c82da83b91e0f492e44cb00bc0e63f84e278
Suraj <kakasuraj22@gmail.com>
2024-08-11 14:52:14 +0530

    Backend Api

---
+++ BookNest/src/test/java/com/Suraj/BookNest/BookNestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Suraj.BookNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
